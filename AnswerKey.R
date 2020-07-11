59dd398dbdab65178d9ec1919cc2f56b971573d5
David Hall <davidross.hall@mail.utoronto.ca>
2020-07-10 21:47:59 -0400

    Single function that produces both CHM135 plots from a single .csv file.

--- AnswerKey.R
+++ AnswerKey.R
@@ -1,22 +1,51 @@
 library(ggplot2)
 library(ggpubr)
+library(ggpmisc)
 library(dplyr)
+library(tidyr)
 library(openxlsx)
 
 ### Specify where to generate the awnser key, I simply created a seperate folder with the CSVs uploaded to Quercus
 setwd(paste(getwd(),"/UploadedToQuercus", sep = ""))
 
 
-data <- read.csv("Toronto_60410_2018_Day2to8.csv", header = TRUE)
+file.list <- list.files( pattern = "\\.csv$", full.names = TRUE)
 
+### Takes in one CHM135 csv files and outputs a single multiplot with Pollutant time series and O3 vs No2 correlation. 
+CHM135Plots <- function(CSVfile){
+  
+data <- read.csv(CSVfile, header = TRUE)
 
 data <- data %>%
-  mutate(Time = convertToDateTime(data$Date, origin = "1900-01-01")) %>%
+  mutate(time = convertToDateTime(data$Date, origin = "1900-01-01")) %>%
   filter(O3 != -999) %>%
   filter(NO2 != -999) %>%
   mutate(OX = NO2 + O3)
 
+formula <- y ~ x ### Need to keep this so LM regression appears on plot
 
-ggscatter(data, x = "NO2", y = "O3", add="reg.line") +
-  stat_cor( aes(label = paste(..rr.label..)),label.x = 0, label.y = 0)+
-  stat_regline_equation(label.x = 0, label.y = 1)
+p <- ggplot(data = data, aes(x = NO2, y = O3)) +
+  geom_point() + 
+  scale_x_continuous(expand = c(0, 0), limits = c(0, NA)) + 
+  scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) +
+  theme_classic() +
+  xlab(bquote('Conc.' ~NO[2]~', ppb')) +
+  ylab(bquote('Conc.' ~O[3]~', ppb')) +
+  geom_smooth(method = "lm", formula = formula, se = FALSE) +
+  stat_poly_eq(aes(label =  paste(stat(eq.label), stat(rr.label), sep = "*\", \"*")),
+               formula = formula, rr.digits = 4 , parse = TRUE, label.y = 0.05)
+
+dataCol <- data %>%
+  select(-c("Date", "temperature")) %>%
+  pivot_longer(-time, names_to = "pollutant", values_to = "concentration")
+
+q <- ggplot(data = dataCol, aes(x = time, y = concentration, color = pollutant)) +
+  geom_line(size = 1) +
+  theme_classic() +
+  ylab(bquote('Conc., ppb')) +
+  xlab(bquote('Time')) 
+
+ggarrange(q, p, 
+          labels = c("A", "B"), 
+          ncol = 1, nrow = 2)
+}
