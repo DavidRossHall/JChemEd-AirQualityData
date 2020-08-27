4204be34eabd4795ee71d9702b22327652755136
David Hall <davidross.hall@mail.utoronto.ca>
2020-08-27 15:47:02 -0400

    the student subsets are now wrapped up in one R file. Now:
    - Option to save data subsets as .csv in new directory
    - Specify number of winter/summer pairs
    - Only requires three inputs (O3, NO2, & NAPSID)

--- dataSubsets.R
+++ dataSubsets.R
@@ -8,14 +8,11 @@ library(stringr)
 library(anytime)
 
 
-O3 <- "O3_2018.csv"
-NO2 <- "NO2_2018.csv"
-weather <- "weatherstats_toronto_hourly.csv"
-year <- 2018
-NAPSID <- 60410
-folder <- 'Toronto_60410_2018/'
-city <- 'Toronto'
-
+O3 <- "O3_2018.csv" # ECCC hourly O3 file
+NO2 <- "NO2_2018.csv" # ECCC hourly NO2 report
+NAPSID <- 60410 # ECCC NAPSID, i.e. location you want data from.
+dataPairs <- 15 # number of paired winter/summer datasets.
+save <- TRUE # TRUE = save files in new directory, FALSE = output list of student data subsets dfs
 
 # 2. cleanUpECCC: function that converts ECCC files into column w/ Excel Dates ---------------------
 cleanUpECCC <- function(file,NAPSID){
@@ -51,25 +48,57 @@ cleanUpECCC <- function(file,NAPSID){
 #     overlap is number of days datasets overlap, default is 144 hrs or 6 days
 
 
-studentData <- function(O3, NO2, NAPSID = 60433 , nsplit=364,overlap=144){
+studentData <- function(O3, NO2, NAPSID = 60433, dataPairs = 15, save = TRUE, nsplit=364,overlap=144){
   
   O3 <- cleanUpECCC(file = O3, NAPSID = NAPSID)
   NO2 <- cleanUpECCC(file = NO2, NAPSID = NAPSID)
   
+  # # extracting values for saving .csvs
+  city <- O3$City[[1]]
+  date <-as.Date(O3$Date[[1]], origin = "1899-12-30")
+  year <- as.numeric(format(date,'%Y'))
+
+  # creating directory for saving .csvs
+  folder <- paste(city,"_",NAPSID,"_",year,"/", sep = "")
+  dir.create(folder)
+  
+  # merging NO2 and O3 dfs  
   merge_df <- merge(NO2, O3, by="Date")
   keep <- c("Date", "NO2", "O3")
   df <- merge_df[keep]
 
+  # generating list of df subsets
+      nrows <- NROW(df)
+      nperdf <- ceiling( (nrows + overlap*nsplit) / (nsplit+1) )
+      start <- seq(1, nsplit*(nperdf-overlap)+1, by= nperdf-overlap )
+    
+      if( start[nsplit+1] + nperdf != nrows )
+        warning("Returning an incomplete dataframe.")
+    
+      lst <- lapply(start, function(i) df[c(i:(i+nperdf-1)),])
+      lst <- lst[c(1:dataPairs, 180:(180+dataPairs))]
+
+  if (save == TRUE){
+    for (i in 1:length(lst)) {
+      
+      tmp <- lst[[i]]
+      tmp[sample(nrow(tmp), 2), 'O3'] <- -999 # every dataset gets -999 error
+      
+      # extracting day of the year for naming files
+      date <-as.Date(tmp$Date[[1]], origin = "1899-12-30")
+      doy <- as.numeric(strftime(date, format = "%j"))
+      
+      fn = paste0(folder,city,"_",NAPSID,"_",year,"_Day",doy,"to",(doy+6),".csv")
+      write.csv(tmp,fn,row.names=FALSE)
+      }
+  } else {
+    return(lst)
+  }
   
-  nrows <- NROW(df)
-  nperdf <- ceiling( (nrows + overlap*nsplit) / (nsplit+1) )
-  start <- seq(1, nsplit*(nperdf-overlap)+1, by= nperdf-overlap )
-
-  if( start[nsplit+1] + nperdf != nrows )
-    warning("Returning an incomplete dataframe.")
-
-  lapply(start, function(i) df[c(i:(i+nperdf-1)),])
 }
 
-lst <- studentData(O3, NO2, NAPSID)
-
+lst <- studentData(O3 = O3,
+                   NO2 = NO2, 
+                   NAPSID = NAPSID,
+                   dataPairs = dataPairs,
+                   save = save)
