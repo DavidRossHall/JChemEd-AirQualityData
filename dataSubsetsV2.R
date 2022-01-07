7fffa6ece652e000b3a0fd3e88f280abf49a967b
David Hall <davidross.hall@mail.utoronto.ca>
2022-01-07 16:04:24 -0500

    drafted student subsetting functions.

--- dataSubsetsV2.R
+++ dataSubsetsV2.R
@@ -48,11 +48,6 @@ readECCC <- function(file){
   ## Option of converting datetime to Excel or POSIX timestamps
   ## ? possible for loop over list of files. clean first file, and join all others via loop? 
 
-O3 <- readECCC(file = O3_2018)
-
-NO2 <- readECCC(file = NO2_2018)
-
-SO2 <- readECCC(file = SO2_2018)
 
 joinECCC <- function(O3, NO2, NAPSID = 60435, excelTimestamp = TRUE){
   
@@ -89,7 +84,7 @@ joinECCC <- function(O3, NO2, NAPSID = 60435, excelTimestamp = TRUE){
     
   } else{
   
-  # Create Time column with POSIC timestamp.      
+  # Create Time column with POSIX timestamp.      
   df <- df %>%
     mutate(Time = paste0(Date, " ", Hour, ":00")) %>%
     mutate(Time = lubridate::parse_date_time(Time, "%Y-%m-%d %H:%M") - lubridate::hours(1)) %>%
@@ -105,12 +100,29 @@ df
 }
 
 
-# 3. Folder location ------------
+# 3. Extracts strings specific to joinECCC subset ------------
 
-folderLocation <- function(x){
+# Creates new folder location from joinECCC formatted data subset
+
+## 3.1 subset city ------------
+subsetCity <- function(x){
   
   city <- x$City[[1]]
+  city
+}
+
+## 3.2 subset NAPS ------------
+
+subsetNAPS <- function(x){
+  
   NAPS <- x$NAPS[[1]]
+  NAPS
+  
+}
+
+## 3.3 subset year ------------
+
+subsetYear <- function(x){
   
   if(is.numeric(x$Time)){
     # if dates stored as Excel timestamp (i.e. numeric)
@@ -120,13 +132,82 @@ folderLocation <- function(x){
     # getting year if POSIX
     year <- format(x$Time[[1]], format = "%Y")
   }
-  # creating directory for saving .csvs
-  folder <- paste(city,"_",NAPS,"_", year, "/", sep = "")
-  folder
+
+year
   
 }
 
-folderLocation(dfPOSIX)
+# 3.4 Subset Day of Year; get's day of year of first line in subset 
+subsetDOY <- function(x){
+  
+  if(is.numeric(x$Time)){
+    # if dates stored as Excel timestamp (i.e. numeric)
+    date <-as.Date(x$Time[[1]], origin = "1899-12-30")
+    doy <- as.numeric(strftime(date, format = "%j"))
+  } else {
+    # getting year if POSIX
+    doy <- format(x$Time[[1]], format = "%j")
+  }
+  
+  doy <- as.numeric(doy)
+  doy
+}
 
+# 3.5 subsetProv 
 
+subsetProv <- function(x){
+  
+  Prov <- x$Province[[1]]
+  Prov
+  
+}
+
+# student data ---------------------
+
+studentData <- function(joinedECCC,dataPairs = 15, save = TRUE, nsplit = 364, overlap=144){
+
+  df <- joinedECCC
+
+  # getting metadata from joined eccc
+  city <- subsetCity(df)
+  year <- subsetYear(df)
+  NAPSID <- subsetNAPS(df)
+  Prov <- subsetProv(df)
+  
+  # removing unnessary columns 
+  df <- df %>% 
+    select(-c("City", "Province", "Latitude", "Longitude"))
+  
+  # creating directory for saving .csvs
+  folder <- paste(city,"_",NAPSID,"_",year,"/", sep = "")
+  dir.create(folder)
+  
+  # generating list of df subsets
+  nrows <- NROW(df)
+  nperdf <- ceiling( (nrows + overlap*nsplit) / (nsplit+1) )
+  start <- seq(1, nsplit*(nperdf-overlap)+1, by= nperdf-overlap )
+  
+  if( start[nsplit+1] + nperdf != nrows )
+    warning("Returning an incomplete dataframe.")
+  
+  lst <- lapply(start, function(i) df[c(i:(i+nperdf-1)),])
+  lst <- lst[c(1:dataPairs, 180:(180+dataPairs))]
+  
+  if (save == TRUE){
+    for (i in 1:length(lst)) {
+      
+      tmp <- lst[[i]]
+      tmp[sample(nrow(tmp), 2), 'O3'] <- -999 # every dataset gets -999 error
+      
+      # extracting day of the year for naming files
+      doy <- subsetDOY(tmp)
+      
+      fn = paste0(folder,city,"_",NAPSID,"_",year,"_Day",doy,"to",(doy+6),".csv")
+      write.csv(tmp,fn,row.names=FALSE)
+    }
+  } else {
+    return(lst)
+  }
+  
+}
 
