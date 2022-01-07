55ea6961591ff2f933f4c8e2f658fdb2c183f579
David Hall <davidross.hall@mail.utoronto.ca>
2022-01-07 18:42:40 -0500

    wrapped up with example dataset

--- AutoAnswerReports.R
+++ AutoAnswerReports.R
@@ -4,15 +4,15 @@
 # An answer key for each .csv is also generated for the CHM 135 Experiment 1. 
 
 
-# 1. Loading library and functions ---------------------------------
+# 1. Loading library and functions ----
 library(tidyverse)
-source("dataSubsetsV2.R") # needs to be in the same directory
+source("dataSubsets.R") # needs to be in the same directory
 
-# 2. Parameters used for the generation of student datasets & answer keys ---------------
+# 2. Parameters used for the generation of student datasets & answer keys ----
 
-O3 <- "raw-data/O3_2018.csv"   # ECCC hourly O3 file
-NO2 <- "raw-data/NO2_2018.csv" # ECCC hourly NO2 report
-NAPSID <- 60433       # ECCC NAPSID, i.e. location you want data from.
+O3 <- "raw-data/O3_2019.csv"   # ECCC hourly O3 file
+NO2 <- "raw-data/NO2_2019.csv" # ECCC hourly NO2 report
+NAPSID <- 60435       # ECCC NAPSID, i.e. location you want data from.
 excelTimestamp <- FALSE
 
 dataPairs <- 15       # number of paired winter/summer datasets.
@@ -21,7 +21,7 @@ save <- TRUE          # TRUE = save files in new directory, FALSE = output list 
 
 
 
-# 3. Generating datasets, if save == TRUE, saves datasets as .csv in new folder ---------------
+# 3. Generating datasets, if save == TRUE, saves datasets as .csv in new folder ----
 
 cityECCC <- joinECCC(O3 = O3, 
                      NO2 = NO2, 
@@ -35,7 +35,7 @@ studentData(joinedECCC =cityECCC,
 
 
 
-# 4. Generating answer keys for saved datasets.
+# 4. Generating answer keys for saved datasets. ----
 
   folder <- folderLocation(cityECCC)
   #setwd(paste0(getwd(),"/",folder, sep=""))
@@ -47,13 +47,15 @@ studentData(joinedECCC =cityECCC,
     
     
     ### Markdown file needs to be in the same directory as the Uplaoded CHM135 .csv files.
-    rmarkdown::render(input = "AnswerKeyV2.Rmd",
+    rmarkdown::render(input = "AnswerKey.Rmd",
                       output_file = paste0(gsub(".csv","", file), ".pdf"),
                       params = list(file = file,
                                     title = sub(".*/", "", file))
     )
   }
 
-# 5. To merge PDF use an external application such as PDFSam
-    # download here: https://pdfsam.org/
+# 5. Merging PDFs ----
+  
+  #To merge PDF use an external application such as PDFSam
+  # download here: https://pdfsam.org
   
